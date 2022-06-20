b989e4cfce41d5d8ac3ab34cfdad5031c15d35bb
pjwan <104919626+pjwan@users.noreply.github.com>
2022-06-21 00:59:29 +0800

    Update Difficulty-Medium.sql

--- Difficulty-Medium.sql
+++ Difficulty-Medium.sql
@@ -75,7 +75,8 @@ ORDER BY freq DESC, id;
 /*
 The total score of a hacker is the sum of their maximum scores for all of the challenges. 
 Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score. 
-If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. Exclude all hackers with a total score of  from your result.
+If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. 
+Exclude all hackers with a total score of 0 from your result.
 */
 -- Subquery version
 SELECT
