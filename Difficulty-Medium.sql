f6a021f6123e200d1320b845c49af8cd9d351024
pjwan <104919626+pjwan@users.noreply.github.com>
2022-06-25 18:34:20 +0800

    Update Difficulty-Medium.sql

--- Difficulty-Medium.sql
+++ Difficulty-Medium.sql
@@ -120,6 +120,7 @@ ORDER BY total_score DESC, hacker_id;
 /* 3.  Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
 Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
 */
+-- subquery version
 SELECT 
     h.hacker_id, 
     h.name
